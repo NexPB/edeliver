cc5954df91edfe2bb30c76ac4c942f0346f7e855
NexPB <1289189+NexPB@users.noreply.github.com>
2025-01-24 13:55:48 +0900

    chore: mix format

--- lib/edeliver.ex
+++ lib/edeliver.ex
@@ -27,7 +27,7 @@ defmodule Edeliver do
   @spec start(term, term) :: {:ok, pid}
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-    children = [worker(__MODULE__, [], [name: __MODULE__])]
+    children = [worker(__MODULE__, [], name: __MODULE__)]
     options = [strategy: :one_for_one, name: Edeliver.Supervisor]
     Supervisor.start_link(children, options)
   end
@@ -48,20 +48,25 @@ defmodule Edeliver do
     name  of the main application and all further arguments are passed to the
     function that's name is equal to the command name.
   """
-  @spec run_command(args::[term]) :: no_return
+  @spec run_command(args :: [term]) :: no_return
   def run_command([:monitor_startup_progress, application_name, :verbose]) do
-    :error_logger.add_report_handler Edeliver.StartupProgress
+    :error_logger.add_report_handler(Edeliver.StartupProgress)
     monitor_startup_progress(application_name)
-    :error_logger.delete_report_handler Edeliver.StartupProgress
+    :error_logger.delete_report_handler(Edeliver.StartupProgress)
   end
+
   def run_command([:monitor_startup_progress, application_name | _]) do
     monitor_startup_progress(application_name)
   end
+
   def run_command([command_name, application_name | arguments]) when is_atom(command_name) do
     application_name = String.to_atom(application_name)
-    {^application_name, _description, application_version} = :application.which_applications |> List.keyfind(application_name, 0)
-    application_version = to_string application_version
-    apply __MODULE__, command_name, [application_name, application_version | arguments]
+
+    {^application_name, _description, application_version} =
+      :application.which_applications() |> List.keyfind(application_name, 0)
+
+    application_version = to_string(application_version)
+    apply(__MODULE__, command_name, [application_name, application_version | arguments])
   end
 
   @doc """
@@ -72,13 +77,18 @@ defmodule Edeliver do
     This can be used as rpc call after running the asynchronous `bin/$APP start` command to
     wait until all applications started and then return `ok`.
   """
-  @spec monitor_startup_progress(application_name::atom) :: :ok
+  @spec monitor_startup_progress(application_name :: atom) :: :ok
   def monitor_startup_progress(application_name) do
-    edeliver_pid = Process.whereis __MODULE__
+    edeliver_pid = Process.whereis(__MODULE__)
+
     if is_pid(edeliver_pid) do
       :ok
     else
-      receive do after 500 -> :ok end
+      receive do
+      after
+        500 -> :ok
+      end
+
       monitor_startup_progress(application_name)
     end
   end
@@ -88,14 +98,21 @@ defmodule Edeliver do
 
     which is either the `:current` version or the `:permanent` version.
   """
-  @spec release_version(application_name::atom, application_version::String.t) :: String.t
+  @spec release_version(application_name :: atom, application_version :: String.t()) :: String.t()
   def release_version(application_name, _application_version \\ nil) do
-    releases = :release_handler.which_releases
-    application_name = Atom.to_charlist application_name
-    case (for {name, version, _apps, status} <- releases, status == :current and name == application_name, do: to_string(version)) do
-      [current_version] -> current_version
+    releases = :release_handler.which_releases()
+    application_name = Atom.to_charlist(application_name)
+
+    case for {name, version, _apps, status} <- releases,
+             status == :current and name == application_name,
+             do: to_string(version) do
+      [current_version] ->
+        current_version
+
       _ ->
-        case (for {name, version, _apps, status} <- releases, status == :permanent and name == application_name, do: to_string(version)) do
+        case for {name, version, _apps, status} <- releases,
+                 status == :permanent and name == application_name,
+                 do: to_string(version) do
           [permanent_version] -> String.to_charlist(permanent_version)
         end
     end
@@ -104,26 +121,45 @@ defmodule Edeliver do
   @doc """
     Prints the pending ecto migrations
   """
-  def list_pending_migrations(application_name, application_version, ecto_repository \\ '') do
+  def list_pending_migrations(application_name, application_version, ecto_repository \\ ~c"") do
     repository = ecto_repository!(application_name, ecto_repository)
     migrator = Ecto.Migrator
     versions = migrator.migrated_versions(repository)
-    pending_migrations = migrations_for(migrations_dir(application_name, application_version))
-    |> Enum.filter(fn {version, _name, _file} -> not (version in versions) end)
-    |> Enum.reverse
-    |> Enum.map(fn {version, name, _file} -> {version, name} end)
-    pending_migrations |> Enum.each(fn {version, name} ->
-      warning "pending: #{name} (#{version})"
+
+    pending_migrations =
+      migrations_for(migrations_dir(application_name, application_version))
+      |> Enum.filter(fn {version, _name, _file} -> version not in versions end)
+      |> Enum.reverse()
+      |> Enum.map(fn {version, name, _file} -> {version, name} end)
+
+    pending_migrations
+    |> Enum.each(fn {version, name} ->
+      warning("pending: #{name} (#{version})")
     end)
   end
 
   @doc """
     Runs the pending ecto migrations
   """
-  def migrate(application_name, application_version, ecto_repository, direction, migration_version \\ :all) when is_atom(direction) do
-    options = if migration_version == :all, do: [all: true], else: [to: to_string(migration_version)]
+  def migrate(
+        application_name,
+        application_version,
+        ecto_repository,
+        direction,
+        migration_version \\ :all
+      )
+      when is_atom(direction) do
+    options =
+      if migration_version == :all, do: [all: true], else: [to: to_string(migration_version)]
+
     migrator = Ecto.Migrator
-    migrator.run(ecto_repository!(application_name, ecto_repository), migrations_dir(application_name, application_version), direction, options)
+
+    migrator.run(
+      ecto_repository!(application_name, ecto_repository),
+      migrations_dir(application_name, application_version),
+      direction,
+      options
+    )
   end
 
   @doc """
@@ -131,7 +167,7 @@ defmodule Edeliver do
   """
   def migrations_dir(application_name, application_version) do
     # use priv dir from installed version
-    lib_dir = :code.priv_dir(application_name) |> to_string |> Path.dirname |> Path.dirname
+    lib_dir = :code.priv_dir(application_name) |> to_string |> Path.dirname() |> Path.dirname()
     application_with_version = "#{Atom.to_string(application_name)}-#{application_version}"
     Path.join([lib_dir, application_with_version, "priv", "repo", "migrations"])
   end
@@ -140,28 +176,49 @@ defmodule Edeliver do
     {:ok, args}
   end
 
-  defp ecto_repository!(_application_name, ecto_repository = [_|_] ) do
+  defp ecto_repository!(_application_name, ecto_repository = [_ | _]) do
     # repository name was passed as ECTO_REPOSITORY env by the erlang-node-execute rpc call
-    List.to_atom ecto_repository
+    List.to_atom(ecto_repository)
   end
+
   defp ecto_repository!(application_name, _ecto_repository) do
-    case System.get_env "ECTO_REPOSITORY" do # ECTO_REPOSITORY env was set when the node was started
-      ecto_repository = <<_,_::binary>> ->
-        ecto_repository_module = ecto_repository |> to_charlist |> List.to_atom
+    # ECTO_REPOSITORY env was set when the node was started
+    case System.get_env("ECTO_REPOSITORY") do
+      ecto_repository = <<_, _::binary>> ->
+        ecto_repository_module = ecto_repository |> to_charlist |> List.to_atom()
+
         if maybe_ecto_repo?(ecto_repository_module) do
           ecto_repository_module
         else
-          error! "Module '#{ecto_repository_module}' is not an ecto repository.\n    Please set the correct repository module in the edeliver config as ECTO_REPOSITORY env\n    or remove that value to use autodetection of that module."
+          error!(
+            "Module '#{ecto_repository_module}' is not an ecto repository.\n    Please set the correct repository module in the edeliver config as ECTO_REPOSITORY env\n    or remove that value to use autodetection of that module."
+          )
         end
+
       _ ->
         case ecto_repos_from_config(application_name) do
-          {:ok, [ecto_repository_module]} -> ecto_repository_module
-          {:ok, modules =[_|_]} -> error! "Found several ecto repository modules (#{inspect modules}).\n    Please specify the repository to use in the edeliver config as ECTO_REPOSITORY env."
+          {:ok, [ecto_repository_module]} ->
+            ecto_repository_module
+
+          {:ok, modules = [_ | _]} ->
+            error!(
+              "Found several ecto repository modules (#{inspect(modules)}).\n    Please specify the repository to use in the edeliver config as ECTO_REPOSITORY env."
+            )
+
           :error ->
-            case Enum.filter(:erlang.loaded |> Enum.reverse, &ecto_1_0_repo?/1) do
-              [ecto_repository_module] -> ecto_repository_module
-              [] -> error! "No ecto repository module found.\n    Please specify the repository in the edeliver config as ECTO_REPOSITORY env."
-              modules =[_|_] -> error! "Found several ecto repository modules (#{inspect modules}).\n    Please specify the repository to use in the edeliver config as ECTO_REPOSITORY env."
+            case Enum.filter(:erlang.loaded() |> Enum.reverse(), &ecto_1_0_repo?/1) do
+              [ecto_repository_module] ->
+                ecto_repository_module
+
+              [] ->
+                error!(
+                  "No ecto repository module found.\n    Please specify the repository in the edeliver config as ECTO_REPOSITORY env."
+                )
+
+              modules = [_ | _] ->
+                error!(
+                  "Found several ecto repository modules (#{inspect(modules)}).\n    Please specify the repository to use in the edeliver config as ECTO_REPOSITORY env."
+                )
             end
         end
     end
@@ -193,14 +250,16 @@ defmodule Edeliver do
   # taken from https://github.com/elixir-lang/ecto/blob/master/lib/ecto/migrator.ex#L183
   defp migrations_for(directory) do
     query = Path.join(directory, "*")
+
     for entry <- Path.wildcard(query),
-      info = extract_migration_info(entry),
-      do: info
+        info = extract_migration_info(entry),
+        do: info
   end
 
   defp extract_migration_info(file) do
     base = Path.basename(file)
-    ext  = Path.extname(base)
+    ext = Path.extname(base)
+
     case Integer.parse(Path.rootname(base)) do
       {integer, "_" <> name} when ext == ".exs" -> {integer, name, file}
       _ -> nil
@@ -208,11 +267,10 @@ defmodule Edeliver do
   end
 
   # defp info(message),    do: IO.puts "==> #{IO.ANSI.green}#{message}#{IO.ANSI.reset}"
-  defp warning(message), do: IO.puts "==> #{IO.ANSI.yellow}#{message}#{IO.ANSI.reset}"
+  defp warning(message), do: IO.puts("==> #{IO.ANSI.yellow()}#{message}#{IO.ANSI.reset()}")
+
   defp error!(message) do
-     IO.puts "==> #{IO.ANSI.red}#{message}#{IO.ANSI.reset}"
-     throw "error"
+    IO.puts("==> #{IO.ANSI.red()}#{message}#{IO.ANSI.reset()}")
+    throw("error")
   end
-
-
 end
